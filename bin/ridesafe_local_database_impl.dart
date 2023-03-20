9531ebbe9eb3cbb84d119cd8b8d7986deb648278
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-20 11:41:19 +0800

    init

---
+++ bin/ridesafe_local_database_impl.dart
@@ -0,0 +1,5 @@
+import 'package:ridesafe_local_database_impl/ridesafe_local_database_impl.dart' as ridesafe_local_database_impl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ridesafe_local_database_impl.calculate()}!');
+}
---
+++ test/ridesafe_local_database_impl_test.dart
@@ -0,0 +1,8 @@
+import 'package:ridesafe_local_database_impl/ridesafe_local_database_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
